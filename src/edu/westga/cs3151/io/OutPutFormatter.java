7b3abac08760a589360a75dea4ea0f84d14ca6ce
jerem <jerem@160.10.105.61>
2019-02-02 20:16:23 -0500

    Here

--- src/edu/westga/cs3151/io/OutPutFormatter.java
+++ src/edu/westga/cs3151/io/OutPutFormatter.java
@@ -9,18 +9,11 @@ public class OutPutFormatter {
 		this.testDriver = new TestDriver();
 	}
 
-	public String generateOutPut() {
-		int steps = 0;
-
-		while (steps <= 20) {
-			int length = 0;
-
-			while (length <= 100) {
-				this.testDriver.setArrayLengthAndPopulate(length);
-
-			}
-
-		}
+	/**
+	 * 
+	 * @return
+	 */
+	public String generateOutPutForInsertionSort() {
 		return "";
 	}
 
