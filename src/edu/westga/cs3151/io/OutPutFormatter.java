c13ef4a905161bf6b8a1b0737df2abc40860af31
jerem <jerem@160.10.105.61>
2019-02-03 17:18:08 -0500

    outPutFormatter here

--- src/edu/westga/cs3151/io/OutPutFormatter.java
+++ src/edu/westga/cs3151/io/OutPutFormatter.java
@@ -15,14 +15,57 @@ public class OutPutFormatter {
 	/**
 	 * Generates output for insertion sort
 	 * 
-	 * @return
+	 * @return the output for insertion sort
 	 */
 	public String generateOutPutForInsertionSort() {
 		String outPut = "";
 		for (int i = 0; i <= STOP; i += STEP) {
-				
+			this.testDriver.setArrayLengthAndPopulate(i);
+			outPut +=  this.testDriver.getTimeOfInsertionSort() + System.lineSeparator();
+
 		}
 		return outPut;
 	}
 
+	/**
+	 * Generates output for selection sort
+	 * 
+	 * @return the output of a selection sort
+	 */
+	public String generateOutPutForSelctionSort() {
+		String outPut = "";
+		for (int i = 0; i <= STOP; i += STEP) {
+			this.testDriver.setArrayLengthAndPopulate(i);
+			outPut += this.testDriver.getTimeOfSelectionSort() + System.lineSeparator();
+
+		}
+		return outPut;
+	}
+
+	/**
+	 * Generate output for a bubble sort
+	 * 
+	 * @return the output of the bubble sort
+	 */
+	public String generateOutPutForBubbleSort() {
+		String outPut = "";
+		for (int i = 0; i <= STOP; i += STEP) {
+			this.testDriver.setArrayLengthAndPopulate(i);
+			outPut += this.testDriver.getTimeOfBubbleSort() + System.lineSeparator();
+
+		}
+		return outPut;
+	}
+	/**
+	 * 
+	 */
+	public String generateOutPutForQuickSort() {
+		String outPut = "";
+		for (int i = 0; i <= STOP; i += STEP) {
+			this.testDriver.setArrayLengthAndPopulate(i);
+			outPut += this.testDriver.getTimeOfQuickSort() + System.lineSeparator();
+
+		}
+		return outPut;
+	}
 }
