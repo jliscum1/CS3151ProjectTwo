92235051a09fc7bdaa0e91bc371e82f5680e0640
jerem <jerem@160.10.105.61>
2019-02-02 20:33:08 -0500

    Building outPut

--- src/edu/westga/cs3151/io/OutPutFormatter.java
+++ src/edu/westga/cs3151/io/OutPutFormatter.java
@@ -4,17 +4,25 @@ import controller.TestDriver;
 
 public class OutPutFormatter {
 	public TestDriver testDriver;
+	private static final int STOP = 100;
+	private static final int STEP = 1;
+	private static final int REPEAT = 20;
 
 	public OutPutFormatter() {
 		this.testDriver = new TestDriver();
 	}
 
 	/**
+	 * Generates output for insertion sort
 	 * 
 	 * @return
 	 */
 	public String generateOutPutForInsertionSort() {
-		return "";
+		String outPut = "";
+		for (int i = 0; i <= STOP; i += STEP) {
+				
+		}
+		return outPut;
 	}
 
 }
