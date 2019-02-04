a0fb95ae6c89d36a17a7718a0b2925000acaef96
jerem <jerem@160.10.105.61>
2019-02-04 11:34:19 -0500

    Setting up main

---
+++ src/main/Main.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package main;
+
+import edu.westga.cs3151.io.OutPutFormatter;
+
+/**
+ * @author jerem
+ *
+ */
+public class Main {
+
+	/**
+	 * Starts Program
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		OutPutFormatter formatter = new OutPutFormatter();
+		
+	}
+
+}
